55aec808e9e521c5b535dcdc0136e2c1080a66d5
laurent dumont <laurent.dumont@student.unamur.be>
2018-12-23 19:10:23 +0100

    Add files via upload
    version 1 des vins de loire

--- loire.pl
+++ loire.pl
@@ -1,21 +1,37 @@
-nom(,).
+nom(chinon_vieilles_vignes_2014,'Chinon Vieilles Vignes 2014').
+nom(sancerre_rouge_2015,'Sancerre rouge 2015').
 
-vignoble(,).
+vignoble(chinon_vieilles_vignes_2014,loire).
+vignoble(sancerre_rouge_2015,loire).
 
-prix(,).
+prix(chinon_vieilles_vignes_2014,8.39).
+prix(sancerre_rouge_2015,15.13).
 
-bouche(,
+bouche(chinon_vieilles_vignes_2014,
+  [
+     ['','.']
+  ]).
+bouche(sancerre_rouge_2015,
   [
      ['','.']
   ]).
 
-
-nez(,
+nez(chinon_vieilles_vignes_2014,
+  [
+     ['.']
+  ]).
+nez(sancerre_rouge_2015,
   [
      ['.']
   ]).
 
-description(,
+
+description(chinon_vieilles_vignes_2014,
   [
      ['', '.']
   ]).
+description(sancerre_rouge_2015,
+  [
+     ['', '.']
+  ]).
+
