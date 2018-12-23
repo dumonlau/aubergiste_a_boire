4b4ddfdb854d24f8906e70fabd61bad519b7a63d
laurent dumont <laurent.dumont@student.unamur.be>
2018-12-23 18:10:50 +0100

    Add files via upload
    ajout bordeau et languedoc.

---
+++ bordeau.pl
@@ -0,0 +1,350 @@
+nom(chateau_moulin_de_mallet_2014,'Ch. Moulin de Mallet 2014 - Bordeaux').
+nom(chateau_la_fleur_baudron_2014, 'Ch. La Fleur Baudron 2014 - Bordeaux Supérieur').
+nom(chateau_bois_vert_2014, 'Ch. Bois Vert "Cuvée Elégance" 2014 ').
+nom(chateau_paret_2012, 'Ch. Paret 2012').
+nom(chateau_menota_2014, 'Ch. Ménota 2014').
+nom(madiran_vieilles_vignes_2006, 'Madiran Vieilles Vignes 2006').
+nom(chateau_du_moulin_neuf_2014, 'Ch. Moulin Neuf 2014').
+nom(chateau_milon_la_grave_2012, 'Ch. Milon La Grave 2012').
+nom(chateau_roc_de_binet_2010, 'Ch. Roc de Binet 2010').
+nom(chateau_ruat_2010, 'Ch. Ruat 2010').
+nom(chateau_les_polyanthas_2010,'Ch. Les Polyanthas 2010').
+nom(chateau_la_menotte_2012, 'Ch. La Menotte 2012').
+nom(la_fleur_de_pomys_2012, 'La Fleur de Pomys 2012').
+nom(florilege_pauillac_2011, 'Florilège Pauillac 2011').
+nom(florilege_saint_julien_2010, 'Florilège Saint Julien 2010').
+nom(florilege_pomerol_2012, 'Florilège Pomerol 2012').
+
+vignoble(chateau_moulin_de_mallet_2014,bordeaux).
+vignoble(chateau_la_fleur_baudron_2014,bordeaux).
+vignoble(chateau_bois_vert_2014,bordeaux).
+vignoble(chateau_paret_2012,bordeaux).
+vignoble(chateau_menota_2014,bordeaux).
+vignoble(madiran_vieilles_vignes_2006,bordeaux).
+vignoble(chateau_du_moulin_neuf_2014,bordeaux).
+vignoble(chateau_milon_la_grave_2012,bordeaux).
+vignoble(chateau_roc_de_binet_2010,bordeaux).
+vignoble(chateau_ruat_2010,bordeaux).
+vignoble(chateau_les_polyanthas_2010,bordeaux).
+vignoble(chateau_la_menotte_2012,bordeaux).
+vignoble(la_fleur_de_pomys_2012,bordeaux).
+vignoble(florilege_pauillac_2011,bordeaux).
+vignoble(florilege_saint_julien_2010,bordeaux).
+vignoble(florilege_pomerol_2012,bordeaux).
+
+
+millesime(les_chaboeufs_2013, 2013).
+millesime(chateau_moulin_de_mallet_2014, 2014).
+millesime(chateau_la_fleur_baudron_2014, 2014).
+millesime(chateau_bois_vert_2014, 2014).
+millesime(chateau_paret_2012, 2012).
+millesime(chateau_menota_2014, 2014).
+millesime(madiran_vieilles_vignes_2006, 2006).
+millesime(chateau_du_moulin_neuf_2014, 2014).
+millesime(chateau_milon_la_grave_2012, 2012).
+millesime(chateau_roc_de_binet_2010, 2010).
+millesime(chateau_ruat_2010, 2010).
+millesime(chateau_les_polyanthas_2010, 2010).
+millesime(chateau_la_menotte_2012, 2012).
+millesime(la_fleur_de_pomys_2012, 2012).
+millesime(florilege_pauillac_2011, 2011).
+millesime(florilege_saint_julien_2010, 2010).
+millesime(florilege_pomerol_2012, 2012).
+
+prix(chateau_moulin_de_mallet_2014, 6.85).
+prix(chateau_la_fleur_baudron_2014, 7.48).
+prix(chateau_bois_vert_2014, 8.11).
+prix(chateau_paret_2012, 8.68).
+prix(chateau_menota_2014, 9.46).
+prix(madiran_vieilles_vignes_2006, 9.92).
+prix(chateau_du_moulin_neuf_2014, 10.03).
+prix(chateau_milon_la_grave_2012, 12.54).
+prix(chateau_roc_de_binet_2010, 12.69).
+prix(chateau_ruat_2010, 14.37).
+prix(chateau_les_polyanthas_2010, 15.06).
+prix(chateau_la_menotte_2012, 15.71).
+prix(la_fleur_de_pomys_2012, 21.36).
+prix(florilege_pauillac_2011, 30.65).
+prix(florilege_saint_julien_2010, 30.65).
+prix(florilege_pomerol_2012, 31.04).
+
+
+bouche(chateau_moulin_de_mallet_2014,
+  [
+     ['un joli vin, classique, polyvalent, avec un bel éventail aromatique et des tannins fins', '.']
+  ]).
+bouche(chateau_la_fleur_baudron_2014,
+  [
+     ['bouche très plaisante, riche, et de beaux tannins associés à un boisé bien intégré','.']
+  ]).
+bouche(chateau_bois_vert_2014,
+  [
+     ['les saveurs sont très généreuses, très plaisantes avec cette rondeur caractéristique du merlot','.']
+  ]).
+bouche(chateau_paret_2012,
+  [
+     ['la bouche suit, alliant matière et densité, avec une grande maturité'],
+     ['on a de belles notes de fruits noirs, de chocolat et de sous-bois (aiguilles de pin)','.']
+  ]).
+bouche(chateau_menota_2014,
+  [
+     ['la texture est ronde et veloutée aux tannins soyeux'],
+     ['l\'ensemble est dense, d\'un remarquable équilibre','.']
+  ]).
+bouche(madiran_vieilles_vignes_2006,
+  [
+     ['le temps a permis à ce cru de s\'épanouir et d\'offrir aujourd\'hui une merveilleuse palette d\'arômes et de saveurs','.']
+  ]).
+bouche(chateau_du_moulin_neuf_2014,
+  [
+     ['témoigne d\'une belle maturité, avec des tannins mûrs','.']
+  ]).
+bouche(chateau_milon_la_grave_2012,
+  [
+     ['les saveurs sont denses et crémeuses, avec beaucoup d\'harmonie','.']
+  ]).
+bouche(chateau_roc_de_binet_2010,
+  [
+     ['sensation de richesse, d\'étoffe, de densité à l\'attaque','.']
+  ]).
+bouche(chateau_ruat_2010,
+  [
+     ['les saveurs sont à la fois charnues, serrées, riches, concentrées et onctueuses','.']
+  ]).
+bouche(chateau_les_polyanthas_2010,
+  [
+     ['le style est onctueux, rond et soyeux, avec une remarquable profondeur','.']
+  ]).
+bouche(chateau_la_menotte_2012,
+  [
+     ['la texture est veloutée, racée, d\'une très grande étoffe','.']
+  ]).
+bouche(la_fleur_de_pomys_2012,
+  [
+     ['les saveurs sont riches et concentrées, avec beaucoup de densité','.']
+  ]).
+bouche(florilege_pauillac_2011,
+  [
+     ['la bouche est puissante, mais les tannins sont élégants et bien accordés','.']
+  ]).
+bouche(florilege_saint_julien_2010,
+  [
+     ['la sensation dense et crémeuse témoigne de la plénitude de ce cru de présent et d\'avenir','.']
+  ]).
+bouche(florilege_pomerol_2012,
+  [
+     ['puissance aromatique se poursuit en bouche avec des saveurs riches et intenses au toucher velouté','.']
+  ]).
+
+
+nez(chateau_moulin_de_mallet_2014,
+  [
+     [cerise, mûre,'.']
+  ]).
+nez(chateau_la_fleur_baudron_2014,
+  [
+     ['fruits compotés',épices,violette,'.']
+  ]).
+nez(chateau_bois_vert_2014,
+  [
+     [bois, violette, cassis, empyreumatique,'.']
+  ]).
+nez(chateau_paret_2012,
+  [
+     ['.']
+  ]).
+nez(chateau_menota_2014,
+  [
+     ['fruits noirs',épices,empyreumatique,'.']
+  ]).
+nez(madiran_vieilles_vignes_2006,
+  [
+     ['fruits noirs',cacao,'.']
+  ]).
+nez(chateau_du_moulin_neuf_2014,
+  [
+     [myrtille, mûre,fumée,chocolat,'.']
+  ]).
+nez(chateau_milon_la_grave_2012,
+  [
+     [fraise, groseille, empyreumatique,'.']
+  ]).
+nez(chateau_roc_du_binet_2010,
+  [
+     [cassis, cerise, violette, truffe, '.']
+  ]).
+nez(chateau_ruat_2010,
+  [
+     ['.']
+  ]).
+nez(chateau_les_polyanthas_2010,
+  [
+     ['.']
+  ]).
+nez(chateau_la_menotte_2012,
+  [
+     [intense, fruits, fleurs, truffe, café, violette,'.']
+  ]).
+nez(la_fleur_de_pomys_2012,
+  [
+     [empyreumatique,complexe, concentré,'.']
+  ]).
+nez(florilege_pauillac_2011,
+  [
+     [concentré,'fruits noirs', 'touche de bois de vigne',réglisse,'.']
+  ]).
+nez(florilege_saint_julien_2010,
+  [
+     ['.']
+  ]).
+nez(florilege_pomerol_2012,
+  [
+     ['bouquet intense','.']
+  ]).
+
+
+robe(chateau_moulin_de_mallet_2014,
+  [
+     [jolie, grenat, 'reflets rubis', '.']
+  ]).
+robe(chateau_la_fleur_baudron_2014,
+  [
+     [soutenue, '.']
+  ]).
+robe(chateau_bois_vert_2014,
+  [
+     ['.']
+  ]).
+robe(chateau_paret_2012,
+  [
+     ['.']
+  ]).
+robe(chateau_menota_2014,
+  [
+     [grenat, profond,'.']
+  ]).
+robe(chateau_madiran_vieilles_vignes_2006,
+  [
+     ['.']
+  ]).
+robe(chateau_du_moulin_neuf_2014,
+  [
+     ['.']
+  ]).
+robe(chateau_milon_la_grave_2012,
+  [
+     [profonde, '.']
+  ]).
+robe(chateau_roc_du_binet_2010,
+  [
+     [profonde, '.']
+  ]).
+robe(chateau_ruat_2010,
+  [
+     ['.']
+  ]).
+robe(chateau_les_polyanthas_2010,
+  [
+     ['.']
+  ]).
+robe(chateau_la_menotte_2012,
+  [
+     [profonde, rouge, velours, '.']
+  ]).
+robe(la_fleur_de_pomys_2012,
+  [
+     [profonde, '.']
+  ]).
+robe(florilege_pauillac_2011,
+  [
+     [velours, '.']
+  ]).
+robe(florilege_saint_julien_2010,
+  [
+     ['.']
+  ]).
+robe(florilege_pomerol_2012,
+  [
+     ['.']
+  ]).
+
+description(chateau_moulin_de_mallet_2014,
+  [
+     ['vignoble situé non loin de Castillon-la-Bataille et des rives de la Dordogne','.'],
+     ['joli vin', '.'],
+     ['vin classique et polyvalent', '.']
+  ]).
+description(chateau_la_fleur_baudron_2014,
+  [
+     ['magnifique terroir à l\'arrière de Lussac-Saint-Emilion reposant sur des sols graveleux et argilo-calcaires','.'],
+     ['un bordeaux très complet', '.']
+  ]).
+description(chateau_bois_vert_2014,
+  [
+     ['appellation de la région de Bordeaux','.'],
+     ['apprécié et réputé particulièrement en Belgique pour la souplesse et la qualité de ses vins','.']
+  ]).
+description(chateau_paret_2012,
+  [
+     ['propriété située dans l\'excellente zone des coteaux de l\'appellation qui jouxte celle de Saint-Emilion','.'],
+     ['beau caractère et laisse le souvenir d\'un vin très complet', '.']
+  ]).
+description(chateau_menota_2014,
+  [
+     ['propriété au sud de Pessac-Léognan en bordure de la Garonne ayant bénéficié d\'un bon amoncellement de graves garonnaises','.'],
+     ['délicieux Graves plein d\'étoffe','.']
+  ]).
+description(madiran_vieilles_vignes_2006,
+  [
+     ['nous vous le recommandons en accompagnement de préparations fortes en goût','.']
+  ]).
+description(chateau_du_moulin_neuf_2014,
+  [
+     ['petite propriété de 2,5 Ha au nord de l\'AOC Médoc','.'],
+     ['terroir graveleux sur fond argilo-ferrugineux','.'],
+     ['belle cuvée qui témoigne de la réussite des cabernets dans le Médoc en 2014','.'],
+     ['un maître achat','.']
+  ]).
+description(chateau_milon_la_grave_2012,
+  [
+     ['cette cuvée composée presque exclusivement de merlot s\'inscrit dans un style très velouté','.']
+  ]).
+description(chateau_roc_du_binet_2010,
+  [
+     ['un magnifique Montagne-Saint-Emilion dans une grande année','.'],
+     ['de présent et d\'avenir', '.']
+  ]).
+description(chateau_ruat_2010,
+  [
+     ['sis sur un terroir de fines graves blanches','.'],
+     ['similaire à de nombreux égards aux terroirs de Margaux', '.'],
+     ['finale de grande classe, longue et distinguée', '.'],
+     ['splendide témoin du Haut-Médoc','.']
+  ]).
+description(chateau_les_polyanthas_2010,
+  [
+     ['le merlot affectionne tout particulièrement les sols de sables et de graves de cette petite propriété de 4 Ha; il y exprime tout sa rondeur, son opulence','.'],
+     ['un Saint-Emilion de grande classe, qui ne pourra que séduire les amateurs de l\'appellation', '.']
+  ]).
+description(chateau_la_menotte_2012,
+  [
+     ['magnifique témoin de l\'appellation','.']
+  ]).
+description(la_fleur_de_pomys_2012,
+  [
+     ['propriété du Nord du Haut-Médoc reposant sur un terroir argilo-graveleux à l\'origine de vins plus charpentés','.']
+  ]).
+description(florilege_pauillac_2011,
+  [
+     ['le compagnon idéal des préparations de gibier','.']
+  ]).
+description(florilege_saint_julien_2010,
+  [
+     ['un remarquable témoin de l\'appellation Saint-Julien avec une trame tannique remarquablement enrobée par le gras, la chair du vin','.'],
+     ['un grand vin racé qui associe concentration, puissance et distinction','.']
+  ]).
+description(florilege_pomerol_2012,
+  [
+     ['issu de sols riches en argile ferrugineuse qui font la spécificité de l\appellation','.'],
+     ['du grand Pomerol qui évoluera encore de nombreuses années', '.']
+  ]).
---
+++ rhone_languedoc.pl
@@ -0,0 +1,239 @@
+vignoble(syrah_2015, rhone).
+vignoble(cotes_rhones_village_2014, rhone).
+vignoble(tautavel_2014, rhone).
+vignoble(lirac_2015, rhone).
+vignoble(cairanne_2014, rhone).
+vignoble(beaumes_venise_2015, rhone).
+vignoble(vacqueyras_2014, rhone).
+vignoble(saint_joseph_2014, rhone).
+vignoble(gigondas_2014, rhone).
+vignoble(chateauneuf_du_pape_rouge_2013, rhone).
+vignoble(hermitage_rouge_2007, rhone).
+vignoble(syrah_2015, languedoc).
+vignoble(cotes_rhones_village_2014,languedoc).
+vignoble(tautavel_2014,languedoc).
+vignoble(lirac_2015,languedoc).
+vignoble(cairanne_2014,languedoc).
+vignoble(beaumes_venise_2015,languedoc).
+vignoble(vacqueyras_2014,languedoc).
+vignoble(saint_joseph_2014,languedoc).
+vignoble(gigondas_2014,languedoc).
+vignoble(chateauneuf_du_pape_rouge_2013,languedoc).
+vignoble(hermitage_rouge_2007,languedoc).
+
+
+nom(syrah_2015,'Syrah 2015 - Vin de pays d\'Oc').
+nom(cotes_rhones_village_2014,'Côtes du Rhônes Villages 2014 "Cuvée Pierre et Charlotte"').
+nom(tautavel_2014, 'Tautavel 2014 - Côtes du Roussillon Villages').
+nom(lirac_2015, 'Lirac 2015').
+nom(cairanne_2014, 'Cairanne 2014 - Côtes du Rhône Villages').
+nom(beaumes_venise_2015, 'Beaumes-de-Venise 2015').
+nom(vacqueyras_2014, 'Vacqueyras 2014').
+nom(saint_joseph_2014, 'Saint-Joseph 2014').
+nom(gigondas_2014, 'Gigondas 2014').
+nom(chateauneuf_du_pape_rouge_2013, 'Châteauneuf-du-Pape rouge 2013').
+nom(hermitage_rouge_2007, 'Hermitage rouge 2007').
+
+prix(syrah_2015, 5.14).
+prix(cotes_rhones_village_2014, 7.36).
+prix(tautavel_2014, 7.37).
+prix(lirac_2015, 9.23).
+prix(cairanne_2014, 9.98).
+prix(beaumes_venise_2015, 12.39).
+prix(vacqueyras_2014, 13.92).
+prix(saint_joseph_2014, 16.34).
+prix(gigondas_2014, 16.77).
+prix(chateauneuf_du_pape_rouge_2013, 23.87).
+prix(hermitage_rouge_2007, 33.98).
+
+bouche(syrah_2015,
+      [
+       ['en bouche, il présente un beau caractère, avec des saveurs de fruits rouges associées à des nuances de réglisses','.']
+      ]).
+bouche(cotes_rhones_village_2014,
+      [
+       ['l\'attaque est tout en fruit avec des saveurs fruitées et épicées, la suite est de caractère, racée avec une nuance de réglisse','.']
+       ]).
+bouche(tautavel_2014,
+      [
+       ['la bouche dévoile d\'abord des saveurs de fruits frais et d\'épices','.'],
+       ['elle est remarquablement concentrée','.'],
+       ['finale cacao, à la fois dense, puissante, juteuse et harmonieuse','.']
+       ]).
+bouche(lirac_2015,
+      [
+       ['la bouche est à la fois dense et minérale, avec des saveurs fruitées et épicées (aiguilles de pin, eucalyptus)','.'],
+       ['la finale laisse un très agréable souvenir','.']
+       ]).
+bouche(cairanne_2014,
+      [
+       ['la bouche est équilibrée avec des tannins soyeux','.'],
+       ['finale de caractère, de belle persistance','.']
+       ]).
+bouche(beaumes_venise_2015,
+      [
+       ['cette intensité se poursuit en bouche avec des saveurs juteuses, racées et très élégantes','.']
+       ]).
+bouche(vacqueyras_2014,
+      [
+       ['les saveurs sont à la fois intenses (fruits surmûris, guimauve, réglisse) et juteuses','.'],
+       ['la finale laisse le souvenir d\'un cru très plaisant, à la fois dense et velouté','.']
+       ]).
+bouche(saint_joseph_2014,
+      [
+       ['remarquable équilibre gras-fraîcheur-matière','.']
+       ]).
+bouche(gigondas_2014,
+      [
+       ['la bouche est racée, riche en saveur complexes (fruits noirs, épices)','.']
+       ]).
+bouche(chateauneuf_du_pape_rouge_2013,
+      [
+       ['Les saveurs sont intenses, racées, riches et complexes','.']
+       ]).
+bouche(hermitage_rouge_2007,
+      [
+       ['grande harmonie en bouche, très homogène, avec une belle profondeur','.'],
+       ['aucune dureté','.']
+       ]).
+
+
+nez(syrah_2015,
+      [
+       ['nef expressif mêlant les fruits rouges, les agrumes à jolies notes d\'épices','.']
+      ]).
+nez(cotes_rhones_village_2014,
+      [
+       ['bouquet plein de charme, très aromatique','.']
+       ]).
+nez(tautavel_2014,
+      [
+       ['bouquet intense de fruits noirs (noyaux) et d\'épices associé à des notes de chocolat et de garrigue','.']
+       ]).
+nez(lirac_2015,
+      [
+       ['nez parfumé, dominé par les fruits à noyaux et les épices (cannelle, réglisse)','.']
+       ]).
+nez(cairanne_2014,
+      [
+       ['les arômes sont très expressifs, très purs, bien typés, évoquant la guimauve, le bois de genévrier, le tabac','.']
+       ]).
+nez(beaumes_venise_2015,
+      [
+       ['les arômes de fraise, de violette, côtoient les nuances de baie de genévrier, de sureau et une délicate touche de fleur d\'oranger','.']
+       ]).
+nez(vacqueyras_2014,
+      [
+       ['les saveurs sont à la fois intenses (fruits surmûris, guimauve, réglisse) et juteuses','.']
+       ]).
+nez(saint_joseph_2014,
+      [
+        ['très aromatique, avec beaucoup de fruit (pamplemousse), des notes d\'épices (poivre) et une nuance empyreumatique (bois brûlé)','.']
+       ]).
+nez(gigondas_2014,
+      [
+       ['le nez riche et concentré, annonce un vin très élégant, avec un fond remarquable','.']
+       ]).
+nez(chateauneuf_du_pape_rouge_2013,
+      [
+       ['le nez est complexe, expressif et suave, associant arômes de fruits rouges, de cannelle et d\'épices (poivre, réglisse)','.'],
+       ['les saveurs sont intenses, racées, riches et complexes','.']
+       ]).
+nez(hermitage_rouge_2007,
+      [
+       ['Nez au bouquet complexe, très profond, associant les fruits noirs, les agrumes à des notes de vanille, de tabac et de réglisse','.']
+       ]).
+
+
+robe(syrah_2015,
+      [
+       ['belle robe rouge profond','.']
+      ]).
+robe(cotes_rhones_village_2014,
+      [
+       ['','.']
+       ]).
+robe(tautavel_2014,
+      [
+       ['robe noire','.']
+       ]).
+robe(lirac_2015,
+      [
+       ['robe grenat profond','.']
+       ]).
+robe(cairanne_2014,
+      [
+       ['couleur cerise du Nord','.']
+       ]).
+robe(beaumes_venise_2015,
+      [
+       ['','.']
+       ]).
+robe(vacqueyras_2014,
+      [
+       ['','.']
+       ]).
+robe(saint_joseph_2014,
+      [
+       ['','.']
+       ]).
+robe(gigondas_2014,
+      [
+       ['','.']
+       ]).
+robe(chateauneuf_du_pape_rouge_2013,
+      [
+       ['','.']
+       ]).
+robe(hermitage_rouge_2007,
+      [
+       ['','.']
+       ]).
+
+
+description(syrah_2015,
+      [
+       ['un élevage sous bois de 12 mois a permis aux tannins de se fondre et ainsi de laisser en finale le souvenir d\'un vin très harmonieux','.']
+      ]).
+description(cotes_rhones_village_2014,
+      [
+       ['magnifique terroir argilo-limoneux à proximité de l\'Aygues','.']
+       ]).
+description(tautavel_2014,
+      [
+       ['','.']
+       ]).
+description(lirac_2015,
+      [
+       ['','.']
+       ]).
+description(cairanne_2014,
+      [
+       ['magnifique terroir à côté de Rasteau, en altitude','.']
+       ]).
+description(beaumes_venise_2015,
+      [
+       ['vignoble situé au sud-est des Dentelles de Montmirail','.']
+       ]).
+description(vacqueyras_2014,
+      [
+       ['les sols caillouteux de Vacqueyras sont à l\'origine de vins chaleureux, fruits et épicés','.']
+       ]).
+description(saint_joseph_2014,
+      [
+       ['vin "typé" syrah de Nord, très aromatique','.'],
+       ['Remarquable équilibre gras-fraîcheur-matière','.']
+       ]).
+description(gigondas_2014,
+      [
+       ['superbe vignoble escarpé situé au pied des Dentelles de Montmirail','.'],
+       ['le terroir est caillouteux et particulièrement adapté à la culture du cépage grenache noir (présent à 80%)','.']
+       ]).
+description(chateauneuf_du_pape_rouge_2013,
+      [
+       ['vignes reposant sur des terrasses du Quaternaire constituée d\'une épaisse couche de galets roulés, à l\'origine de vins puissants et charnus','.']
+       ]).
+description(hermitage_rouge_2007,
+      [
+       ['vin de grande allure, racé, qui s\'épanouit superbement','.']
+       ]).
