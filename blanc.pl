84ab1504acce8347973681713ebde4f8ce8677bb
laurent dumont <laurent.dumont@student.unamur.be>
2018-12-23 19:02:13 +0100

    Add files via upload

---
+++ blanc.pl
@@ -0,0 +1,262 @@
+nom(les_guignards_2015,'Les Guignards 2015').
+nom(chardonnay_exception_2016,'Chardonnay Exception 2016').
+nom(cote_rhone_2016,'Côtes du Rhône 2016 "Cuvée Pierre et Charlotte"').
+nom(le_druc_2015,'Ch. Le Druc 2015 - Graves').
+nom(laudun_2016,'Laudun 2016 - Côtes du Rhône Villages').
+nom(vouvray_blanc_sec_2016,'Vouvray blanc sec 2016').
+nom(macon_villages_2015,'Mâcon-Villages 2015').
+nom(pinot_gris_2015,'Pinot Gris 2015').
+nom(gewurztraminer_2015,'Gewurztraminer 2015').
+nom(vire_clesse_2016,'Viré-Clessé 2016').
+nom(sancerre_blanc_2015,'Sancerre blanc 2015 - Vignoble de Verdigny').
+nom(vacqueyras_2016,'Vacqueyras 2016').
+nom(hautes_cotes_de_beaune_2015,'Hautes Côtes de Beaune 2015').
+nom(pouilly_fuisse_2014,'Pouilly-Fuissé 2014').
+nom(chablis_1_cru_montmains_2014,'Chablis 1er Cru "Montmains" 2014').
+nom(condrieu_2015,'Condrieu 2015').
+
+
+vignoble(les_guignards_2015,blanc).
+vignoble(chardonnay_exception_2016,blanc).
+vignoble(cote_rhone_2016,blanc).
+vignoble(le_druc_2015,blanc).
+vignoble(laudun_2016,blanc).
+vignoble(vouvray_blanc_sec_2016,blanc).
+vignoble(macon_villages_2015,blanc).
+vignoble(pinot_gris_2015,blanc).
+vignoble(gewurztraminer_2015,blanc).
+vignoble(vire_clesse_2016,blanc).
+vignoble(sancerre_blanc_2015,blanc).
+vignoble(vacqueyras_2016,blanc).
+vignoble(hautes_cotes_de_beaune_2015,blanc).
+vignoble(pouilly_fuisse_2014,blanc).
+vignoble(chablis_1_cru_montmains_2014,blanc).
+vignoble(condrieu_2015,blanc).
+
+prix(les_guignards_2015,5.30).
+prix(chardonnay_exception_2016,6.47).
+prix(cote_rhone_2016,7.02).
+prix(le_druc_2015,7.62).
+prix(laudun_2016,8.83).
+prix(vouvray_blanc_sec_2016,10.10).
+prix(macon_villages_2015,10.59).
+prix(pinot_gris_2015,10.59).
+prix(gewurztraminer_2015,12.28).
+prix(vire_clesse_2016,12.77).
+prix(sancerre_blanc_2015,16.01).
+prix(vacqueyras_2016,16.37).
+prix(hautes_cotes_de_beaune_2015,16.58).
+prix(pouilly_fuisse_2014,18.67).
+prix(chablis_1_cru_montmains_2014,21.13).
+prix(condrieu_2015,32.07).
+
+
+bouche(les_guignards_2015,
+  [
+     ['','.']
+  ]).
+bouche(chardonnay_exception_2016,
+  [
+     ['','.']
+  ]).
+bouche(cote_rhone_2016,
+  [
+     ['','.']
+  ]).
+bouche(le_druc,
+  [
+     ['','.']
+  ]).
+bouche(laudun_2016,
+  [
+     ['','.']
+  ]).
+bouche(vouvray_blanc_sec_2016,
+  [
+     ['','.']
+  ]).
+bouche(macon_villages_2015,
+  [
+     ['','.']
+  ]).
+bouche(pinot_gris_2015,
+  [
+     ['','.']
+  ]).
+bouche(gewurztraminer_2015,
+  [
+     ['','.']
+  ]).
+bouche(vire_clesse_2016,
+  [
+     ['','.']
+  ]).
+bouche(sancerre_blanc_2015,
+  [
+     ['','.']
+  ]).
+bouche(vacqueyras_2016,
+  [
+     ['','.']
+  ]).
+bouche(hautes_cotes_de_beaune_2015,
+  [
+     ['','.']
+  ]).
+bouche(pouilly_fuisse_2014,
+  [
+     ['','.']
+  ]).
+bouche(chablis_1_cru_montmains_2014,
+  [
+     ['','.']
+  ]).
+bouche(condrieu_2015,
+  [
+     ['','.']
+  ]).
+
+
+
+nez(les_guignards_2015,
+  [
+     ['','.']
+  ]).
+nez(chardonnay_exception_2016,
+  [
+     ['','.']
+  ]).
+nez(cote_rhone_2016,
+  [
+     ['','.']
+  ]).
+nez(le_druc,
+  [
+     ['','.']
+  ]).
+nez(laudun_2016,
+  [
+     ['','.']
+  ]).
+nez(vouvray_blanc_sec_2016,
+  [
+     ['','.']
+  ]).
+nez(macon_villages_2015,
+  [
+     ['','.']
+  ]).
+nez(pinot_gris_2015,
+  [
+     ['','.']
+  ]).
+nez(gewurztraminer_2015,
+  [
+     ['','.']
+  ]).
+nez(vire_clesse_2016,
+  [
+     ['','.']
+  ]).
+nez(sancerre_blanc_2015,
+  [
+     ['','.']
+  ]).
+nez(vacqueyras_2016,
+  [
+     ['','.']
+  ]).
+nez(hautes_cotes_de_beaune_2015,
+  [
+     ['','.']
+  ]).
+nez(pouilly_fuisse_2014,
+  [
+     ['','.']
+  ]).
+nez(chablis_1_cru_montmains_2014,
+  [
+     ['','.']
+  ]).
+nez(condrieu_2015,
+  [
+     ['','.']
+  ]).
+
+
+
+description(les_guignards_2015,
+  [
+     ['','.']
+  ]).
+description(chardonnay_exception_2016,
+  [
+     ['','.']
+  ]).
+description(cote_rhone_2016,
+  [
+     ['','.']
+  ]).
+description(le_druc,
+  [
+     ['','.']
+  ]).
+description(laudun_2016,
+  [
+     ['','.']
+  ]).
+description(vouvray_blanc_sec_2016,
+  [
+     ['','.']
+  ]).
+description(macon_villages_2015,
+  [
+     ['','.']
+  ]).
+description(pinot_gris_2015,
+  [
+     ['','.']
+  ]).
+description(gewurztraminer_2015,
+  [
+     ['','.']
+  ]).
+description(vire_clesse_2016,
+  [
+     ['','.']
+  ]).
+description(sancerre_blanc_2015,
+  [
+     ['','.']
+  ]).
+description(vacqueyras_2016,
+  [
+     ['','.']
+  ]).
+description(hautes_cotes_de_beaune_2015,
+  [
+     ['','.']
+  ]).
+description(pouilly_fuisse_2014,
+  [
+     ['','.']
+  ]).
+description(chablis_1_cru_montmains_2014,
+  [
+     ['','.']
+  ]).
+description(condrieu_2015,
+  [
+     ['','.']
+  ]).
+
+
+
+
+
+
+
+
+
+
+
