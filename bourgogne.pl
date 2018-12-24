55f5aac75454f7014b0de5348c7a7054221886c5
laurent dumont <laurent.dumont@student.unamur.be>
2018-12-24 08:24:40 +0100

    Add files via upload

---
+++ bourgogne.pl
@@ -0,0 +1,28 @@
+nom(coteaux_bourguignons_2014,'Coteaux Bourguignons 2014').
+nom(bourgogne_pinot_noir_2014,'Bourgogne Pinot Noir 2014 - Cuvée les Marnes').
+nom(hautes_cotes_nuits_2014,'Hautes Côtes de Nuits 2014').
+nom(savigny_les_beaune_2014,'Savigny-lès-Beaune 2014').
+nom(savigny_les_beaune_1_cru_2014,'Savigny-lès-Beaune 1er Cru 2014').
+nom(aloxe_corton_2014,'Aloxe-Corton 2014').
+nom(nuits_saint_georges_1_cru_2013,'Nuits-Saint-Georges 1er Cru 2013').
+
+
+vignoble(,).
+
+prix(,).
+
+bouche(,
+  [
+     ['','.']
+  ]).
+
+
+nez(,
+  [
+     ['.']
+  ]).
+
+description(,
+  [
+     ['', '.']
+  ]).
